9872ef4d813772f6a4103d1ca77a3c941e2b728a
KU Lok Sun <sundaykuloksun@gmail.com>
2018-09-04 11:28:49 +0800

    Test get object field with Proxy

--- es6/proxy/Duck.js
+++ es6/proxy/Duck.js
@@ -1,4 +1,10 @@
 class Duck {
+  construtor() {
+    this.name = 'Donald';
+  }
+  getName() {
+    return 'Donald';
+  }
   fly() {
     console.log('I am flying!');
   }
--- es6/proxy/index.js
+++ es6/proxy/index.js
@@ -4,9 +4,16 @@ const Duck = require('./Duck.js');
 const traceMethodCalls = require('./traceMethodCalls.js');
 
 const duck = new Duck();
+console.log(`I am ${duck.name}`);
+console.log(`I am ${duck.getName()}`);
 duck.fly();
 duck.quack();
 
 const tracedDuck = traceMethodCalls(duck);
+// This print the function return by proxy getter, it means function call is
+// get object method as a object variable -> call the function got
+// Therefore, the method call will trigger handler's get method
+console.log(`I am ${tracedDuck.name}`);
+console.log(`I am ${tracedDuck.getName()}`);
 tracedDuck.fly();
 tracedDuck.quack();
